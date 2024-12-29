b1272dce972c936621f48f63f383641261e38ddd
Namitha <namithac193@gmail.com>
2024-12-29 09:39:57 +0530

    chatbot

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_rasa_response(user_message):
         return []
 
 # Initialize the Streamlit app
-st.title("Customized Chatbot")
+st.title("Chatbot for Personalized learning")
 
 # Load LMS data
 lms_data = load_lms_data()
